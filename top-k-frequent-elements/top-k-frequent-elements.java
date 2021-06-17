1821979b2336625145802acd90f52d5e11a2b00b
prabhakaran302 <prabhakaran.nivanil@gmail.com>
2021-06-17 18:55:26 +0530

    Time: 14 ms (22.84%), Space: 41.3 MB (88.86%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.java
+++ top-k-frequent-elements/top-k-frequent-elements.java
@@ -8,25 +8,19 @@ class Solution {
 			map.put(i, map.getOrDefault(i, 0) + 1);
 		}
 
-		Comparator<Map.Entry<Integer, Integer>> comp = new Comparator<Map.Entry<Integer, Integer>>() {
-			@Override
-			public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
-				return o1.getValue().compareTo(o2.getValue());
-			}
-		};
-		PriorityQueue<Map.Entry<Integer, Integer>> q = new PriorityQueue<Map.Entry<Integer, Integer>>(comp);
-		for (Map.Entry<Integer, Integer> m : map.entrySet()) {
-			q.offer(m);
-			if (q.size() > k)
-				q.remove();
-		}
+		Map<Integer, Integer> ml = map.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
 		int[] res = new int[k];
-		int c = 0;
-		for (Map.Entry<Integer, Integer> m : q)
-			res[c++] = m.getKey();
-
-		return res;
+		int index = 0;
+		for (Map.Entry<Integer, Integer> m : ml.entrySet()) {
+            res[index] = m.getKey();
+            index++;
+            if(index == k)
+                break;
+        }
+        
+        return res;
 	
     }
 }
