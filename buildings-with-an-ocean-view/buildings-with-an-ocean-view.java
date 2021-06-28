4c8917d495b13a68def4b020f7af5a20b1ba77dc
prabhakaran302 <prabhakaran.nivanil@gmail.com>
2021-06-28 12:43:49 +0530

    Time: 1 ms (100.00%), Space: 56.6 MB (19.81%) - LeetHub

--- buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
+++ buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
@@ -1,23 +1,18 @@
 class Solution {
     public int[] findBuildings(int[] heights) {
-        List<Integer> li = new ArrayList<>();
         int max = heights[heights.length - 1];
-        li.add(heights.length - 1);
+        
+        int [] ans = new int[heights.length];
+        int counter = heights.length - 1;
+        ans[counter--] = heights.length - 1;
         
         for(int ind = heights.length - 2; ind  >= 0; ind--)   {
             if(heights[ind] > max)  {
                 max = heights[ind];
-                li.add(ind);
+                ans[counter--] = ind;
             }
         }
         
-        Collections.reverse(li);
-        
-        int [] ans = new int[li.size()];
-        int index = 0;
-        for(int cur : li)
-            ans[index++] = cur;
-        
-        return ans;
+        return Arrays.copyOfRange(ans, counter+1, ans.length);
     }
 }
