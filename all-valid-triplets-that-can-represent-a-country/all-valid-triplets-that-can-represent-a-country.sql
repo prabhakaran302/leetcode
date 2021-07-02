3da247049b8c182c95397bf488d256d2e445d334
prabhakaran302 <prabhakaran.nivanil@gmail.com>
2021-07-02 09:40:47 +0530

    Time: 2007 ms (48.32%), Space: 0B (100.00%) - LeetHub

---
+++ all-valid-triplets-that-can-represent-a-country/all-valid-triplets-that-can-represent-a-country.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select A.student_name  as member_A,  B.student_name  as member_B, C.student_name  as member_C from SchoolA A, SchoolB B, SchoolC C where A.student_name  != B.student_name  && A.student_name  != C.student_name  && B.student_name  != C.student_name  && A.student_id != B.student_id && B.student_id != C.student_id && A.student_id != C.student_id;
