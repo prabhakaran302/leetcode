55e6b63d2afd6ce6bbc4b07556107a05172e67ed
prabhakaran302 <prabhakaran.nivanil@gmail.com>
2021-08-12 10:50:35 +0530

    Time: 4 ms (10.93%), Space: 39.4 MB (40.02%) - LeetHub

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -1,26 +1,27 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
 		List<List<Integer>> list = new ArrayList<>();
-		permute(nums, list, 0, nums.length - 1);
+		permuteUtil(nums, list, 0, nums.length - 1);
 		return list;
 	}
 
-	private void permute(int[] nums, List<List<Integer>> list, int low, int high) {
+	private void permuteUtil(int[] nums, List<List<Integer>> list, int low, int high) {
 		if (low == high) {
-			list.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+			List<Integer> l = Arrays.stream(nums).boxed().collect(Collectors.toList());
+			list.add(l);
+			return;
 		} else {
 			for (int i = low; i <= high; i++) {
-				nums = swap(nums, i, low);
-				permute(nums, list, low + 1, high);
-				nums = swap(nums, i, low);
+				swap(nums, i, low);
+				permuteUtil(nums, list, low + 1, high);
+				swap(nums, i, low);
 			}
 		}
 	}
 
-	private int[] swap(int[] nums, int i, int low) {
-		int temp = nums[i];
-		nums[i] = nums[low];
-		nums[low] = temp;
-		return nums;
+	private void swap(int[] tempar, int i, int low) {
+		int temp = tempar[i];
+		tempar[i] = tempar[low];
+		tempar[low] = temp;
 	}
 }
