c60945f5a36b4a11364ad767ab116015016981d4
prabhakaran302 <prabhakaran.nivanil@gmail.com>
2021-08-15 18:16:54 +0530

    Time: 30 ms (5.49%), Space: 40.8 MB (10.80%) - LeetHub

--- top-k-frequent-words/top-k-frequent-words.java
+++ top-k-frequent-words/top-k-frequent-words.java
@@ -1,29 +1,26 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        List<String> result = new LinkedList<>();
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<words.length; i++)
-        {
-            if(map.containsKey(words[i]))
-                map.put(words[i], map.get(words[i])+1);
-            else
-                map.put(words[i], 1);
-        }
         
-        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
-                 (a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
-        );
-        
-        for(Map.Entry<String, Integer> entry: map.entrySet())
-        {
-            pq.offer(entry);
-            if(pq.size()>k)
-                pq.poll();
-        }
+		
+		List<String> result = new LinkedList<>();
+		Map<String, Integer> map = new TreeMap<>();
+		for (int i = 0; i < words.length; i++) {
+			map.put(words[i], map.getOrDefault(words[i], 0) + 1);
+		}
+        System.out.println(map);
+		LinkedHashMap<String,Integer> m1 = map.entrySet().parallelStream().sorted((a, b) -> b.getValue().compareTo(a.getValue()))
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        System.out.println(map);
 
-        while(!pq.isEmpty())
-            result.add(0, pq.poll().getKey());
-        
-        return result;
+		Iterator<String> it = m1.keySet().iterator();
+		for (int i = 0; i < k; i++) {
+			if (it.hasNext())
+				result.add(it.next());
+			else
+				break;
+		}
+		return result;
+	
+	
     }
 }
