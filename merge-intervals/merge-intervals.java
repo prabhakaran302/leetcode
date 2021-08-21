d2fbde6313d6a82e78f3f7b4a8229892f91d0ee3
prabhakaran302 <prabhakaran.nivanil@gmail.com>
2021-08-21 12:54:46 +0530

    Time: 7 ms (28.51%), Space: 41.9 MB (38.58%) - LeetHub

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -1,32 +1,40 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-    
-	    
-		int[][] res = new int[intervals.length][intervals[0].length];
-		int length = 0;
-
-		Comparator<int[]> cmp = new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return ((Integer) o1[0]).compareTo((Integer) o2[0]);
-			}
-		};
-
-		Arrays.parallelSort(intervals, cmp);
-		res[length][0] = intervals[0][0];
-		res[length][1] = intervals[0][1];
-
-		for (int i = 1; i < intervals.length; i++) {
-			if (res[length][1] >= intervals[i][0]) {
-				res[length][1] = Math.max(intervals[i][1], res[length][1]);
-			} else {
-				res[length + 1][0] = intervals[i][0];
-				res[length + 1][1] = intervals[i][1];
-				length++;
-			}
-		}
-
-		return Arrays.copyOf(res, length + 1);
-	
+        Arrays.sort(intervals, (a,b)->a[0]-b[0]);
+        
+        
+        
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> l = new ArrayList<>();
+        l.add(intervals[0][0]);
+        l.add(intervals[0][1]);
+        list.add(l);
+        
+        int start = 1;
+        int prev = 0;
+        
+        while(start < intervals.length) {
+            if(list.get(prev).get(1) >= intervals[start][0]) {
+                int prevE = list.get(prev).get(1);
+                list.get(prev).set(1, Math.max(prevE, intervals[start][1]));
+            } else  {
+                l = new ArrayList<>();
+                l.add(intervals[start][0]);
+                l.add(intervals[start][1]);
+                list.add(l);
+                prev = list.size()-1;
+            }
+            start++;
+        }
+        
+        
+        int[][] res = new int[list.size()][2];
+        int index = 0;
+        for(List<Integer> tl : list)   {
+            res[index][0] = tl.get(0);
+            res[index][1] = tl.get(1);
+            index++;
+        }
+        return res;
     }
 }
