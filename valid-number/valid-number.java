8b2b93ede9168a1854186112024acd99321c190a
stranger302 <prabhakaran.nivanil@gmail.com>
2021-09-14 10:22:28 +0530

    Time: 4 ms (29.31%), Space: 39.4 MB (30.98%) - LeetHub

--- valid-number/valid-number.java
+++ valid-number/valid-number.java
@@ -3,42 +3,30 @@ class Solution {
         
 		boolean seenDigit = false;
 		boolean seenExpo = false;
-		boolean seenSign = false;
 		boolean seenDot = false;
 
 		for (int i = 0; i < s.length(); i++) {
-			
 			if (Character.isDigit(s.charAt(i))) {
 				seenDigit = true;
-			} else if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-				if (seenSign)
-					return false;
-
-				if (i == 0) {
-					seenSign = true;
+			} else if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+				if (i == 0)
 					continue;
-				}
-
-				if (s.charAt(i - 1) == 'e' || s.charAt(i - 1) == 'E') {
-					seenSign = true;
-					continue;
-				}
-                else {
+				if(s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E')
 					return false;
-				}
 			} else if (s.charAt(i) == 'e' || s.charAt(i) == 'E') {
-				if (seenExpo || !seenDigit)
+                if (!seenDigit)
+					return false;
+				if (seenExpo)
 					return false;
 				seenExpo = true;
 				seenDigit = false;
-				seenSign = false;
 			} else if (s.charAt(i) == '.') {
-				if (seenDot || seenExpo)
+				if (seenExpo || seenDot)
 					return false;
 				seenDot = true;
-			} else
-				return false;
-		
+			} else  {
+                return false;
+            }
 		}
 
 		return seenDigit;
