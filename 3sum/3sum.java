ca164bf5e7f6b4faeb10e1b9ae1a797d049bdabd
stranger302 <prabhakaran.nivanil@gmail.com>
2021-09-15 16:07:24 +0530

    Time: 808 ms (10.87%), Space: 101.6 MB (7.35%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,32 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         
-        List<List<Integer>> list = new ArrayList<>();
-        if(nums.length < 3)
-            return list;
-        
-        
-        //-4,-1,-1,0,1,2
-        for(int i = 0; i < nums.length && nums[i] <= 0;i++)   {
-            if(i == 0 || nums[i-1] != nums[i])  {
-                int low = i+1;
-                int high = nums.length - 1;
-                while(low < high)   {
-                
-                    if(nums[i] + nums[low] + nums[high] == 0)   {
-                        list.add(Arrays.asList(nums[i], nums[low++], nums[high--]));
-                        while(low < high && nums[low-1] == nums[low])
-                            low++;
-                    }
-                    else if(nums[i] + nums[low] + nums[high] > 0)
-                        high--;
-                    else
-                        low++;
-                }   
-            }
-        }
-        System.out.println(list);
-        return list;
+		Set<List<Integer>> list = new HashSet<>();
+		Map<Integer, Integer> map = new HashMap<>();
+
+		Set<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			if (set.add(nums[i])) {
+				for (int j = i + 1; j < nums.length; j++) {
+					int sum = -nums[i] - nums[j];
+					if (map.containsKey(nums[j]) && map.get(nums[j]) == i) {
+						List<Integer> l = new ArrayList<Integer>();
+						l.add(nums[i]);
+						l.add(nums[j]);
+						l.add(sum);
+						
+						Collections.sort(l);
+						
+						list.add(l);
+					}
+					map.put(sum, i);
+				}
+			}
+		}
+
+		return new ArrayList<>(list);
+	
     }
 }
