8f8dab80c869120a766ed3d08bdc91590c86d324
stranger302 <prabhakaran.nivanil@gmail.com>
2021-09-16 21:30:05 +0530

    Time: 12 ms (34.91%), Space: 41.1 MB (12.22%) - LeetHub

--- valid-palindrome/valid-palindrome.java
+++ valid-palindrome/valid-palindrome.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
         StringBuilder sb = new StringBuilder();
-        for(char cur : s.toCharArray())
-            if(Character.isLetterOrDigit(cur))
-                sb.append(cur);
+        for(char c : s.toCharArray()) {
+            if(Character.isLetterOrDigit(c))
+                sb.append(c);
+            
+        }
         
-        
-        StringBuilder sbrev = new StringBuilder(sb.toString());
-        return sbrev.reverse().toString().equals(sb.toString());
+        return sb.toString().toLowerCase().equals(sb.reverse().toString().toLowerCase());
     }
 }
