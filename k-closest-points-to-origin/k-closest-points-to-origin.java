2da0cc75a2771e52ac2666bfbc703473a2a98587
stranger302 <prabhakaran.nivanil@gmail.com>
2021-09-18 18:32:35 +0530

    Time: 50 ms (17.69%), Space: 115 MB (9.49%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.java
+++ k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,32 +1,18 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-		KClosest comp = new KClosest();
-		PriorityQueue<int[]> q = new PriorityQueue<>(comp);
-		int[][] res = new int[k][2];
-		for (int[] point : points) {
-			q.offer(point);
-			if (q.size() > k) {
-				q.poll();
-			}
-		}
-
-		int index = 0;
-		while (!q.isEmpty()) {
-			int cur[] = q.poll();
-			res[index][0] = cur[0];
-			res[index][1] = cur[1];
-			index++;
-		}
-
-		return res;
-	}
-
-	class KClosest implements Comparator<int[]> {
-
-		@Override
-		public int compare(int[] o1, int[] o2) {
-			return (o2[0] * o2[0] + o2[1] * o2[1] - o1[0] * o1[0] - o1[1] * o1[1]);
-		}
-
-	}
+        Queue<int[]> q = new PriorityQueue<>((a,b)->b[0]*b[0]+b[1]*b[1] - a[0]*a[0] - a[1]*a[1]);
+        
+        for(int[] point : points)   {
+            q.offer(point);
+            if(q.size() > k)
+                q.poll();
+        }
+        
+        int res[][] = new int[q.size()][2];
+        int index = q.size() - 1;
+        while(!q.isEmpty())
+            res[index--] = q.poll();
+        
+        return res;
+    }
 }
