8058dee1feabf7bad977d224b2d9066dd3f9cfcf
stranger302 <prabhakaran.nivanil@gmail.com>
2021-09-19 15:47:29 +0530

    Time: 11 ms (66.02%), Space: 38.5 MB (86.14%) - LeetHub

--- similar-string-groups/similar-string-groups.java
+++ similar-string-groups/similar-string-groups.java
@@ -1,45 +1,43 @@
 class Solution {
     public int numSimilarGroups(String[] strs) {
         
-		int answer = 0;
-		Queue<String> q = new LinkedList<>();
+		int ans = 0;
 		boolean[] visited = new boolean[strs.length];
-
-		for (int index = 0; index < strs.length; index++) {
-			if (visited[index])
+		for (int i = 0; i < strs.length; i++) {
+			if (visited[i])
 				continue;
-			visited[index] = true;
-			answer += 1;
 
-			q.offer(strs[index]);
+			ans++;
+			visited[i] = true;
+
+			Queue<String> q = new LinkedList<>();
+			q.offer(strs[i]);
 
 			while (!q.isEmpty()) {
-				String curString = q.poll();
-				for (int innerLoopIndex = 0; innerLoopIndex < strs.length; innerLoopIndex++) {
-					if (visited[innerLoopIndex])
+				String cur = q.poll();
+				for (int j = 0; j < strs.length; j++) {
+					if (visited[j])
 						continue;
-                    
-                    if (curString.equals(strs[innerLoopIndex])) {
-						visited[innerLoopIndex] = true;
-						continue;
-					}
 
+					String nw = strs[j];
 					int diff = 0;
-					for (int charIndex = 0; charIndex < curString.length(); charIndex++) {
-						if (curString.charAt(charIndex) != strs[innerLoopIndex].charAt(charIndex))
+					for (int k = 0; k < Math.min(nw.length(), cur.length()); k++) {
+						if (cur.charAt(k) != nw.charAt(k))
 							diff++;
 						if (diff > 2)
 							break;
 					}
-					if (diff == 2) {
-						visited[innerLoopIndex] = true;
-						q.offer(strs[innerLoopIndex]);
+                        
+                    
+					if(diff == 2 || diff == 0)	{
+						q.offer(strs[j]);
+						visited[j] = true;
 					}
+
 				}
 			}
 		}
-
-		return answer;
+		return ans;
 	
     }
 }
