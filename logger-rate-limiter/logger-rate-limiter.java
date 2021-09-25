0de508fef38fb0b4f39baea5e687649c3c868a73
stranger302 <prabhakaran.nivanil@gmail.com>
2021-09-25 21:43:40 +0530

    Time: 61 ms (21.03%), Space: 71.6 MB (15.34%) - LeetHub

--- logger-rate-limiter/logger-rate-limiter.java
+++ logger-rate-limiter/logger-rate-limiter.java
@@ -1,27 +1,20 @@
 class Logger {
-
     Map<String,Integer> map;
-    /** Initialize your data structure here. */
     public Logger() {
         map = new HashMap<>();
     }
     
-    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity. */
     public boolean shouldPrintMessage(int timestamp, String message) {
         if(map.containsKey(message))    {
-            if(timestamp >= map.get(message))    {
-               map.put(message, timestamp+10);
-                return true; 
+            if(timestamp - map.get(message) >= 10)   {
+                map.put(message, timestamp);
+                return true;
             } else  {
                 return false;
             }
-            
-        } else  {
-            map.put(message, timestamp+10);
-            return true;
         }
+        map.put(message, timestamp);
+        return true;
     }
 }
 
