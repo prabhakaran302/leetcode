4e216b6cc3f57cdf6ef23086b33c4c379d693146
stranger302 <prabhakaran.nivanil@gmail.com>
2021-09-25 21:16:04 +0530

    Time: 3 ms (44.23%), Space: 43.4 MB (25.17%) - LeetHub

---
+++ intersection-of-three-sorted-arrays/intersection-of-three-sorted-arrays.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+        
+        List<Integer> list = new ArrayList<>();
+        
+        while(p1 < arr1.length && p2 < arr2.length && p3 < arr3.length) {
+            if(arr1[p1] == arr2[p2] && arr3[p3] == arr2[p2])    {
+                list.add(arr1[p1]);
+                p1++;
+                p2++;
+                p3++;
+            } else  {
+                if(arr1[p1] < arr2[p2]) {
+                    p1++;
+                } else if(arr2[p2] < arr3[p3])  {
+                    p2++;
+                } else  {
+                    p3++;
+                }
+            }
+            
+        }
+        
+        return list;
+    }
+}
