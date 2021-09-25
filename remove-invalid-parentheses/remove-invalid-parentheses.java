4ebad50d1bd958fe7931e1db27c50b5b55ca3806
stranger302 <prabhakaran.nivanil@gmail.com>
2021-09-25 12:38:10 +0530

    Time: 52 ms (62.81%), Space: 40.4 MB (26.41%) - LeetHub

--- remove-invalid-parentheses/remove-invalid-parentheses.java
+++ remove-invalid-parentheses/remove-invalid-parentheses.java
@@ -1,63 +1,52 @@
 class Solution {
-    public List<String> removeInvalidParentheses(String s) 
-    {
-        Queue<String> q = new LinkedList();
-        Set<String> visited = new HashSet();
-        q.add(s);
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> result = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        q.offer(s);
+        set.add(s);
         
-        List<String> result = new ArrayList();
         boolean found = false;
-        while (!q.isEmpty())
-        {
-            String curr = q.poll();
-            if (is_valid(curr))
-            {
-                result.add(curr);
-                found = true;
+        
+        while(!q.isEmpty()) {
+            String cur = q.poll();
+            if(isValid(cur))    {
+                result.add(cur);
+                found  = true;
             }
             
-            if (!found)
-            {
-                next(q, visited, curr);
+            if(!found)  {
+                for(int i = 0; i < cur.length();i++)  {
+                    if(cur.charAt(i) == '(' || cur.charAt(i) == ')')    {
+                        String ns = cur.substring(0,i) + cur.substring(i+1);
+                        if(!set.contains(ns))    {
+                            q.offer(ns);
+                            set.add(ns);
+                        }
+                    }
+                    
+                } 
             }
         }
         
         return result;
     }
     
-    void next(Queue<String> q, Set<String> visited, String s)
-    {
-        for (int i = 0; i < s.length(); ++i)
-        {
-            if (s.charAt(i) == '(' || s.charAt(i) == ')')
-            {
-                String n = s.substring(0, i) + s.substring(i + 1);
-                if (!visited.contains(n))
-                {
-                    q.add(n);
-                    visited.add(n);
-                }
-            }
-        }
-    }
-    
-    boolean is_valid(String s)
-    {
-        int count = 0;
-        for (char c : s.toCharArray())
-        {
-            if (c == '(')
-            {
-                ++count;
-            }
-            else if (c == ')')
-            {
-                --count;
-                if (count == -1) return false;
+    public boolean isValid(String str)  {
+        int low = 0;
+        int high = str.length() - 1;
+        
+        int open = 0;
+        for(int i = 0; i <= high;i++)   {
+            if(str.charAt(i) == '(')    {
+                open++;
+            } else if(str.charAt(i) == ')') {
+                open--;
+                if(open == -1)
+                    return false;
             }
         }
         
-        return count == 0;
+        return open == 0;
     }
-    
 }
