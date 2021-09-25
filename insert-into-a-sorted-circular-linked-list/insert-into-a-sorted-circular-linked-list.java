e1de80a4a7b652c2815fa52071bd249394234f12
stranger302 <prabhakaran.nivanil@gmail.com>
2021-09-25 15:51:15 +0530

    Time: 0 ms (100.00%), Space: 38.2 MB (87.23%) - LeetHub

--- insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.java
+++ insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.java
@@ -19,43 +19,35 @@ class Node {
 
 class Solution {
     public Node insert(Node head, int insertVal) {
+        if(head == null)    {
+            Node n = new Node(insertVal);
+            n.next = n;
+            return n;
+        }
+        Node prev = head;
+        Node next = head.next;
         
-		if (head == null) {
-			Node n = new Node(insertVal);
-			n.next = n;
-			return n;
-		}
-
-		Node aHead = head;
-
-		Node cur = head.next;
-		Node prev = head;
-
-		boolean insert = false;
-
-		while (cur != aHead) {
-			if (prev.val <= insertVal && cur.val >= insertVal)
-				insert = true;
-
-			if (prev.val > cur.val)
-				if (prev.val <= insertVal || cur.val >= insertVal)
-					insert = true;
-
-			if (insert) {
-				Node n = new Node(insertVal);
-				prev.next = n;
-				n.next = cur;
-				return aHead;
-			}
-			prev = cur;
-			cur = cur.next;
-		}
-
-		Node n = new Node(insertVal);
-		prev.next = n;
-		n.next = cur;
-		
-		return aHead;
-	
+        boolean toInsert = false;
+        
+        while(next != head)    {
+            if(prev.val <= insertVal && next.val >= insertVal)    {
+                toInsert = true;
+            } else if(prev.val > next.val) {
+                if (insertVal >= prev.val || insertVal <= next.val)
+                    toInsert = true;
+            }
+            
+            
+            if(toInsert)    {
+                prev.next = new Node(insertVal, next);
+                return head;
+            }
+            
+            prev = next;
+            next = next.next;
+        }
+        
+        prev.next = new Node(insertVal, next);
+        return head;
     }
 }
