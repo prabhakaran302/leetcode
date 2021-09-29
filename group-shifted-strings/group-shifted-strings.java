38d1ca16aca12ead423b87d39f2a2a60b5473d51
stranger302 <prabhakaran.nivanil@gmail.com>
2021-09-29 14:34:36 +0530

    Time: 9 ms (25.68%), Space: 39.3 MB (66.69%) - LeetHub

--- group-shifted-strings/group-shifted-strings.java
+++ group-shifted-strings/group-shifted-strings.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<String>> groupStrings(String[] strings) {
-        List<List<String>> result = new ArrayList<>();
-        Map<String, List<String>> map = new TreeMap<>();
+        List<List<String>> list = new ArrayList<>();
+        Map<String,List<String>> map = new HashMap<>();
         for(String str : strings)   {
             String key = getKey(str);
-            map.computeIfAbsent(key, x->new ArrayList<>()).add(str);
+            map.computeIfAbsent(key, x -> new ArrayList<>()).add(str);
         }
-        for(Map.Entry<String, List<String>> en : map.entrySet())
-            result.add(en.getValue());
-        return result;
+        System.out.println(map);
+        for(Map.Entry<String,List<String>> en :  map.entrySet())    {
+            list.add(en.getValue());
+        }
+        
+        return list;
     }
     
-    public String getKey(String str)  {
-        
-		if (str.length() == 1)
-			return "single";
-
-		char[] cs = str.toCharArray();
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i < cs.length; i++) {
-			int diff = cs[i] - cs[i - 1];
-			if (diff < 0)
-				diff += 26;
-			sb.append(diff + ",");
-		}
-		return sb.substring(0, sb.length() - 1);
-	
+    public String getKey(String s)  {
+        StringBuilder sb = new StringBuilder();
+        sb.append("0,");
+        for(int i = 1; i < s.length(); i++)   {
+            int diff = s.charAt(i) - s.charAt(i-1);
+            if(diff < 0)
+                diff += 26;
+            sb.append(diff+",");
+        }
+        return sb.substring(0, sb.length()-1).toString();
     }
 }
