c0a32337fddac072d5b18c83cb3e540fa8d37174
stranger302 <prabhakaran.nivanil@gmail.com>
2021-10-01 11:16:26 +0530

    Time: 1 ms (58.22%), Space: 38.9 MB (65.09%) - LeetHub

--- verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
+++ verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
@@ -1,34 +1,30 @@
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
-		Map<Character, Integer> map = new HashMap<>();
-		int index = 0;
-		for (char c : order.toCharArray()) {
-			map.put(c, index++);
-		}
-
-		for (int i = 1; i < words.length; i++) {
-			String w1 = words[i - 1];
-			String w2 = words[i];
-			if (!isAlienSortedUtil(w1, w2, map)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private boolean isAlienSortedUtil(String str1, String str2, Map<Character, Integer> map) {
-		if (str1.length() > str2.length() && str1.startsWith(str2))
-			return false;
-
-		for (int i = 0; i < Math.min(str1.length(), str2.length()); i++) {
-			if (str1.charAt(i) != str2.charAt(i)) {
-				if(map.get(str1.charAt(i)) > map.get(str2.charAt(i)))
+        Map<Character,Integer> map = new HashMap<>();
+        int ind = 0;
+        for(char c : order.toCharArray())   {
+            map.put(c,ind++);
+        }
+        
+        for(int i = 1; i < words.length;i++)    {
+            if(compare(words[i-1], words[i], map) == false)
+                return false;
+        }
+        return true;
+    }
+    
+    public boolean compare(String w1, String w2, Map<Character,Integer> map)    {
+        if(w1.startsWith(w2) && w1.length() > w2.length())
+            return false;
+        
+        for(int i = 0; i < Math.min(w1.length(),w2.length());i++) {
+            if(w1.charAt(i) != w2.charAt(i))    {
+                if(map.get(w1.charAt(i)) > map.get(w2.charAt(i)))
                     return false;
                 else
                     return true;
-                    
-			}
-		}
-		return true;
-	}
+            }
+        }
+        return true;
+    }
 }
