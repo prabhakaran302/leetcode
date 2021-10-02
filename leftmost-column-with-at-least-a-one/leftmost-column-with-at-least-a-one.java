206e910ea942c050e6d54af218f7430ffb1ff6ac
stranger302 <prabhakaran.nivanil@gmail.com>
2021-10-02 17:23:30 +0530

    Time: 1 ms (26.54%), Space: 44.5 MB (28.17%) - LeetHub

--- leftmost-column-with-at-least-a-one/leftmost-column-with-at-least-a-one.java
+++ leftmost-column-with-at-least-a-one/leftmost-column-with-at-least-a-one.java
@@ -9,17 +9,21 @@
 
 class Solution {
     public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
-        List<Integer> li = binaryMatrix.dimensions();
-        int row = 0;
-        int col = li.get(1) - 1;
+        List<Integer> dim = binaryMatrix.dimensions();
         
-        while(row < li.get(0) && col >= 0)  {
-            if(binaryMatrix.get(row,col) == 1)
-                col--;
-            else
+        int row = 0;
+        int col = dim.get(1) - 1;
+        
+        int prev = col;
+        
+        while(row < dim.get(0) && col >= 0) {
+            if(binaryMatrix.get(row,col) == 0)  {
                 row++;
+            } else if(binaryMatrix.get(row,col) == 1)  {
+                col--;
+            }
         }
         
-        return col == li.get(1) - 1 ? -1 : col + 1;
+        return col == dim.get(1) - 1 ? -1 : col + 1; 
     }
 }
